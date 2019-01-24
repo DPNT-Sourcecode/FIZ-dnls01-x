d03e7decb96390d8214eb16eebc1e89118e72002
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-01-25 00:28:24 +0200

    Fri Jan 25 00:28:24 EET 2019

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -11,28 +11,22 @@ namespace BeFaster.App.Solutions.FIZ
             if (number >= 1 && number <= 9999)
             {
                 string numberAsString = Convert.ToString(number);
-                if ((number % 3 == 0) && (number % 5 == 0))
+                if ((number % 3 == 0 || numberAsString.Contains("3")) && (number % 5 == 0 || numberAsString.Contains("5")))
+                {
+                    return "fizz buzz";
+                }
+                else if ((number % 3 == 0) && (number % 5 == 0))
                 {
 
                     return "fizz buzz";
                 }
-                else if (numberAsString.Contains("3") && numberAsString.Contains("5"))
-                {
-                    return "fizz buzz";
-                }
-                else if (number % 3 == 0)
+                
+                else if (number % 3 == 0 || numberAsString.Contains("3"))
                 {
                     return "fizz";
                 }
-                else if (numberAsString.Contains("3"))
-                {
-                    return "fizz";
-                }
-                else if (number % 5 == 0)
-                {
-                    return "buzz";
-                }
-                else if (numberAsString.Contains("5"))
+               
+                else if (number % 5 == 0 || numberAsString.Contains("5"))
                 {
                     return "buzz";
                 }
