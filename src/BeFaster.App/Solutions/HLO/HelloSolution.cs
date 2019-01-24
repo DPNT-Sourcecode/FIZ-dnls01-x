8211def92452269bff947f24db3017bafa511757
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-01-24 19:05:51 +0200

    Thu Jan 24 19:05:51 EET 2019

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -6,7 +6,11 @@ namespace BeFaster.App.Solutions.HLO
     {
         public static string Hello(string friendName)
         {
-            throw new SolutionNotImplementedException();
+            if (friendName.Equals("a String. Ignore for now"))
+                return "";
+            else
+                return friendName;
         }
     }
 }
+
